5795f50280af8e4631a0a962627c167f53e3b158
nckbrrs <nckbrrs@icloud.com>
2022-02-19 18:42:39 -0500

    added express server for redis operations

--- src/index.tsx
+++ src/index.tsx
@@ -6,32 +6,33 @@ import { WindowSizes } from './types/types';
 import useWindowSize from './assets/useWindowSize';
 import { WindowSizeContext } from './assets/windowSizeContext';
 import './styles/globals.scss';
+require('dotenv').config();
 
 const App:React.FC = () => {
 	const windowSize: WindowSizes = useWindowSize();
 
 	return (
-			<div id="App" className="App">
-				<BrowserRouter>
-					<Switch>
-						<Route exact path="/invitee" render={() => (
-							<WindowSizeContext.Provider value={windowSize}>
-								<AddInvitee hasPlusOne={false}/>
-							</WindowSizeContext.Provider>
-						)}/>
-						<Route exact path="/invitees" render={() => (
-							<WindowSizeContext.Provider value={windowSize}>
-								<AddInvitee hasPlusOne={true}/>
-							</WindowSizeContext.Provider>
-						)}/>
-						<Route path="*" render={() => (
-							<WindowSizeContext.Provider value={windowSize}>
-								<Redirect to="/invitee"/>
-							</WindowSizeContext.Provider>
-						)}/>
-					</Switch>
-				</BrowserRouter>
-			</div>
+		<div id="App" className="App">
+			<BrowserRouter>
+				<Switch>
+					<Route exact path="/invitee" render={() => (
+						<WindowSizeContext.Provider value={windowSize}>
+							<AddInvitee hasPlusOne={false}/>
+						</WindowSizeContext.Provider>
+					)}/>
+					<Route exact path="/invitees" render={() => (
+						<WindowSizeContext.Provider value={windowSize}>
+							<AddInvitee hasPlusOne={true}/>
+						</WindowSizeContext.Provider>
+					)}/>
+					<Route path="*" render={() => (
+						<WindowSizeContext.Provider value={windowSize}>
+							<Redirect to="/invitee"/>
+						</WindowSizeContext.Provider>
+					)}/>
+				</Switch>
+			</BrowserRouter>
+		</div>
 	)
 }
 
