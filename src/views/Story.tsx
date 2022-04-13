81c0a39e5200b30ff9c8ccf80e85250c28509b1f
Nick-Barrs <nckbrrs@icloud.com>
2022-04-13 08:19:42 -0400

    added nick's story and tweaked story layout and css

--- src/views/Story.tsx
+++ src/views/Story.tsx
@@ -3,9 +3,7 @@ import SiteHeader from '../components/SiteHeader';
 import ContentHeader from '../components/ContentHeader';
 import SiteFooter from '../components/SiteFooter';
 import '../styles/story.scss';
-import nbPicBday from '../assets/images/nickAndBrooke/birthday.jpeg';
-import nbPicNA from '../assets/images/nickAndBrooke/na.jpeg';
-import nbPicLap from '../assets/images/nickAndBrooke/lap.jpeg';
+import nbPicIris from '../assets/images/nickAndBrooke/iris.jpg';
 import nbPicMountain from '../assets/images/nickAndBrooke/mountain.jpeg';
 
 const Story: React.FC = () => {
@@ -14,47 +12,66 @@ const Story: React.FC = () => {
             <SiteHeader/>
             <ContentHeader title={"OUR STORY"}/>
             <div id="story-container">
-                <div className="story-photos first">
-                    <div className="img-container">
-                        <img id="nbPicNA" src={nbPicNA}/>
-                    </div>
-                    <div className="img-container">
-                        <img id="nbPicBday" src={nbPicBday}/>
-                    </div>
-                    <div className="img-container">
-                        <img id="nbPicLap" src={nbPicLap}/>
-                    </div>
-                    <div className="img-container">
-                        <img id="nbPicMountain" src={nbPicMountain}/>
+                <div className="img-container">
+                    <div className="img-frame">
+                        <img src={nbPicIris}/>
                     </div>
                 </div>
-                <div id="story-text">
-                    <p className="content-title">Written from Brooke's perspective...</p>
-                    <p className="content-body">
-                        June 2020, I knew I was going to marry Nick. I just woke up one day and felt with all the certainty in my heart that he was who I would spend the rest of my days with.
-                        <br/><br/>
-                        It's amusing that we served in the same ministry together all those years, just out of reach and right where we needed to be. We both served with Clemson FCA for four years, went to the same church, and basically had all the same friends. It's incredible how an acquaintance can change your life.
-                        <br/><br/>
-                        When I moved to Raleigh in April of 2020, I figured I'd spend a few months stuck inside with my friends Nate and Zoë and maybe hang out with Nick as he was only one of 4 humans I knew in the area. Little did we know that COVID was here to stay, and our group would spend the whole next year together three times a week playing Catan and watching Superstore. 
-                        <br/><br/>
-                        We were just friends; that's what I kept telling everyone and myself. But by September, I knew I was in love with him after a weekend trip to visit my family and my hometown. He sent us the demo of the song he'd been working on for months (have you heard it? it's incredible) as soon as we got back to Raleigh, and I knew I was really in trouble. So now I was just left praying our flirty friendship was actually as flirty as I thought.
-                        <br/><br/>
-                        Fast forward to November, and my work was planning to bring everyone out to California for our end-of-the-year Christmas party. I offered a fun long weekend in LA to our group, and Nick was the only one to take me up on it. So, of course, Zoë and I spent the majority of the month leading up to the trip wondering if this was even real and what, if anything, it could mean.
-                        <br/><br/>
-                        But it was real, and it took everything in me to contain my excitement. After a few days in Orange County working with my team, Nick flew out to join me. We stayed in a pretty sketch Airbnb, visited some friends in San Diego, introduced him to a few of my friends, explored as much of LA as we could, and ate a lot of In-N-Out and Sidecar donuts. We hit our groove that Monday celebrating his 25th birthday and decided that we wanted to do that day pretty much all over again on our final day in SoCal. 
-                        <br/><br/>
-                        We ended our last day watching the sunset with Sidecar donuts on the beach at Santa Monica, then picked up In-N-Out to eat up at Griffith Observatory. We chatted and ate dinner on a little trail in front of Griffith, and something just felt right. And I worked up every ounce of courage I had to ask him if anything was going on between us. Thank God he said yes. My little heart soared. And as we walked back up to our rental car, he told me he really wanted to kiss me, and that is the story of the most romantic first kiss in human history with the LA skyline as the backdrop. 
-                        <br/><br/>
-                        Pretty much every day since then has been magic. Before we even made it official, I told him I loved him on the couch he would one day propose to me beside. He told me he loved me a few months later in his car outside a bar in his hometown. 
-                        <br/><br/>
-                        In the last year and some change, we've learned a lot about loving each other well, the beauty of the mundanity of life when you get to spend it with your best friend, and how sweet life really can be. We learned how to navigate a pandemic and massive political and social upheaval together (did I mention we started dating the day of the capital insurrection? lol), and we've learned how to come back to church after a year of couch church. There is truly no one I would have rather had by my side through the good and bad of the last two years. And man, there's no one I'd rather have by my side through whatever life throws at us in decades to come.
-                        <br/><br/>
-                        On February 12, my best friend and my family were in town so I could go dress shopping—yes, we were planning this wedding before he put a ring on it, chill—and he asked us to stop by his place before we all went out to dinner. And in the living room where we fell in love, he asked me to marry him. The best day of my life. That is until June 24. 
-                    </p>
+                <div id="stories">
+                    <div className="story-text">
+                        <p className="content-title">from Brooke's perspective...</p>
+                        <p className="content-body">
+                            June 2020, I knew I was going to marry Nick. I just woke up one day and felt with all the certainty in my heart that he was who I would spend the rest of my days with.
+                            <br/><br/>
+                            It's amusing that we served in the same ministry together all those years, just out of reach and right where we needed to be. We both served with Clemson FCA for four years, went to the same church, and basically had all the same friends. It's incredible how an acquaintance can change your life.
+                            <br/><br/>
+                            When I moved to Raleigh in April of 2020, I figured I'd spend a few months stuck inside with my friends Nate and Zoë and maybe hang out with Nick as he was only one of 4 humans I knew in the area. Little did we know that COVID was here to stay, and our group would spend the whole next year together three times a week playing Catan and watching Superstore. 
+                            <br/><br/>
+                            We were just friends; that's what I kept telling everyone and myself. But by September, I knew I was in love with him after a weekend trip to visit my family and my hometown. He sent us the demo of the song he'd been working on for months (have you heard it? it's incredible) as soon as we got back to Raleigh, and I knew I was really in trouble. So now I was just left praying our flirty friendship was actually as flirty as I thought.
+                            <br/><br/>
+                            Fast forward to November, and my work was planning to bring everyone out to California for our end-of-the-year Christmas party. I offered a fun long weekend in LA to our group, and Nick was the only one to take me up on it. So, of course, Zoë and I spent the majority of the month leading up to the trip wondering if this was even real and what, if anything, it could mean.
+                            <br/><br/>
+                            But it was real, and it took everything in me to contain my excitement. After a few days in Orange County working with my team, Nick flew out to join me. We stayed in a pretty sketch Airbnb, visited some friends in San Diego, introduced him to a few of my friends, explored as much of LA as we could, and ate a lot of In-N-Out and Sidecar donuts. We hit our groove that Monday celebrating his 25th birthday and decided that we wanted to do that day pretty much all over again on our final day in SoCal. 
+                            <br/><br/>
+                            We ended our last day watching the sunset with Sidecar donuts on the beach at Santa Monica, then picked up In-N-Out to eat up at Griffith Observatory. We chatted and ate dinner on a little trail in front of Griffith, and something just felt right. And I worked up every ounce of courage I had to ask him if anything was going on between us. Thank God he said yes. My little heart soared. And as we walked back up to our rental car, he told me he really wanted to kiss me, and that is the story of the most romantic first kiss in human history with the LA skyline as the backdrop. 
+                            <br/><br/>
+                            Pretty much every day since then has been magic. Before we even made it official, I told him I loved him on the couch he would one day propose to me beside. He told me he loved me a few months later in his car outside a bar in his hometown. 
+                            <br/><br/>
+                            In the last year and some change, we've learned a lot about loving each other well, the beauty of the mundanity of life when you get to spend it with your best friend, and how sweet life really can be. We learned how to navigate a pandemic and massive political and social upheaval together (did I mention we started dating the day of the capital insurrection? lol), and we've learned how to come back to church after a year of couch church. There is truly no one I would have rather had by my side through the good and bad of the last two years. And man, there's no one I'd rather have by my side through whatever life throws at us in decades to come.
+                            <br/><br/>
+                            On February 12, my best friend and my family were in town so I could go dress shopping—yes, we were planning this wedding before he put a ring on it, chill—and he asked us to stop by his place before we all went out to dinner. And in the living room where we fell in love, he asked me to marry him. The best day of my life. That is, until June 24. 
+                        </p>
+                    </div>
+                    <div className="story-text">
+                        <p className="content-title">from Nick's perspective...</p>
+                        <p className="content-body">
+                            It’s safe to say that 2020 was full of surprises.
+                            <br/><br/>
+                            Work went remote, church went virtual, and spending time in public became all but impossible. The name of the game was basically to find a few friends and hang out with them inside for the rest of eternity. Luckily, a close friend of mine and her husband (Nate and Zoë) had recently moved to Raleigh. And even more luckily (luckier than I could have ever imagined), Brooke decided to move to Raleigh, too! Even though we weren’t really friends at all, Brooke and I were both close to Nate and Zoe in our own ways, so naturally, the four of us became inseparable — ordering takeout, watching movies and tv shows, playing board games, traveling here and there, etc pretty much every single day of our lives. If you happened to follow either of us on Instagram at the time, you were aware.
+                            <br/><br/>
+                            As you’d expect with the whole single-guy-and-single-girl-hanging-out-with-married-couple situation, the "iS aNyThInG gOiNg On BeTwEeN yOu AnD bRoOkE??"s started to pour in from friends and family. Our external responses would always be "no", but little did we both know how much our internal feelings had been stirring.
+                            <br/><br/>
+                            Well, little did *I* know. Per Brooke’s story, she clearly knew much earlier than I did. Oops!
+                            <br/><br/>
+                            In the Spring, I was pretty fresh out of a long-term relationship (like, on-and-off-for-eight-years long), and I was still pretty new to my city, new to my job, new to my church, new to my friend group (minus Nate + Zoe)… you get the picture. I’d moved a few hundred miles away from pretty much all of my friends and family and was building a new life here on my own.
+                            <br/><br/>
+                            All that to say — I wasn’t really in a place to jump into a new relationship. I wasn’t even looking. So even as Brooke and I became closer, and the questions started pouring in, and the "wow she’s really cool" thoughts started coming on, I kind of didn’t allow my brain to "go there" for a while. It would have been the easiest thing in the world to just say "welp, here’s this pretty girl who went to college with me and is friends with my friends and is quaranting with me and another married couple, etc, etc, etc, we may as well date!" The convenience factor was so strong. But I didn’t want to date someone out of convenience, I didn’t want to start a new relationship just because it was the easy thing to do, and I didn’t want to ruffle our friendship and our friend group. And important to keep in mind — I didn’t know if she liked me!
+                            <br/><br/>
+                            So, that’s where I was at for a few months. And then when I went to California with her, it was just amplified. Pretty much every five minutes I found myself thinking "wow this is really nice?", and "should I hold her hand?", and "uhh is this romantic or am I just way too in my head?" At one point I resolved to, again, shut myself off from those thoughts and think about it more when we got home. I didn’t want her to think I was a creep and I didn’t want to ruin the trip. Luckily, Brooke was (is) way braver than me. On our last night in California, we were sitting on a bench outside Griffith Observatory, eating In-N-Out burgers, looking out over the Los Angeles skyline, when she now famously asked me "is anything going on here?".
+                            <br/><br/>
+                            We proceeded to confess to each other that yes, indeed there was something going on here, we kissed, we flew home, and the rest is history.
+                            <br/><br/><br/>
+                            There have been days in my life where I didn’t think I would ever be ready to be married. I’ve been scared and doubtful and anxious about sharing my life with someone forever and ever.
+                            <br/><br/>
+                            But I’m not that way anymore. Over the past year I’ve come to love Brooke and be loved by her in ways that I didn’t know were possible. She is the light of my life. She’s my dream girl. I know without a doubt that marrying her will be the greatest honor and joy of my life.
+                            <br/><br/>
+                            y'all mind if we get married?
+                        </p>
+                    </div>
                 </div>
-                <div className="story-photos second">
-                    <div className="img-container">
-                        <img id="nbPicMountain2" src={nbPicMountain}/>
+                <div className="img-container">
+                    <div className="img-frame">
+                        <img src={nbPicMountain}/>
                     </div>
                 </div>
             </div>
